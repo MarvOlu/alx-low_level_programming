195dc3fc4ed9d6dddbdfbc802ff0b6f2b4c13a4c
Maxwel Oluoch <oluoch542@gmail.com>
2022-07-01 05:44:21 -0700

    checked

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,24 @@
 
 int main (void)
 {
+	
 	int n;
+
+	srand(time(0));
+	n =rand() - RAND_MAX / 2;
 	if (n>0)
 	{
-		puts("%d is positive",n);
+		puts("%d is positive \n",n);
 	}
-	elseif(n<0)
+	else if(n<0)
 	{
-		puts("d is negative" ,n);
+		puts("%d is negative\n" ,n);
 	}
-	else(num=0){
-		puts("%d is zero", n);
+	else
+		puts("%d is zero\n", n);
 	}
+        
+
+	return (0);
+}
+
