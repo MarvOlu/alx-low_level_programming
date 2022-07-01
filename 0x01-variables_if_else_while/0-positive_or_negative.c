8b0816f446c4b23fa0852480d3e541d2276f301d
Maxwel Oluoch <oluoch542@gmail.com>
2022-07-01 05:18:03 -0700

    checked

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int n;
+	if (n>0)
+	{
+		puts("%d is positive",n);
+	}
+	elseif(n<0)
+	{
+		puts("d is negative" ,n);
+	}
+	else(num=0){
+		puts("%d is zero", n);
+	}
